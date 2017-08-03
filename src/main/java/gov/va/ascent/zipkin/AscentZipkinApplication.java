74bc90d3cd28b0b1f93dd3c72dc6b064caa471b7
prashanthmds <prashanthmds@gmail.com>
2017-08-03 14:37:39 +0200

    ST-1003:Moving ascent-zipkin code base to ascent-zipkin repository

---
+++ src/main/java/gov/va/ascent/zipkin/AscentZipkinApplication.java
@@ -0,0 +1,20 @@
+package gov.va.ascent.zipkin;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+//import org.springframework.scheduling.annotation.EnableAsync;
+
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+@EnableDiscoveryClient
+public class AscentZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AscentZipkinApplication.class, args);
+    }
+}
---
+++ src/test/java/gov/va/ascent/zipkin/AscentZipkinApplicationTests.java
@@ -0,0 +1,16 @@
+package gov.va.ascent.zipkin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AscentZipkinApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
