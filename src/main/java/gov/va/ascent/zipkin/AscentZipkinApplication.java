0901cfe31e09b3f974eb06225e3cdefbbbd6c3af
Abhijit Kulkarni <akulkarni@meetveracity.com>
2017-10-12 16:03:07 -0400

    Cleaned up for unused imports

--- src/main/java/gov/va/ascent/zipkin/AscentZipkinApplication.java
+++ src/main/java/gov/va/ascent/zipkin/AscentZipkinApplication.java
@@ -4,8 +4,6 @@ package gov.va.ascent.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
-//import org.springframework.scheduling.annotation.EnableAsync;
 
 import zipkin.server.EnableZipkinServer;
 
