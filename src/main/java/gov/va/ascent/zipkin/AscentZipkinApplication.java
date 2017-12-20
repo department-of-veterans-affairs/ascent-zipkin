49f33dce6ed72f99ff171b0cacb0ad997afbe770
Nate Paulus <npaulus@meetveracity.com>
2017-12-19 19:22:02 -0600

    zipkin updates for stream server using rabbitmq

--- src/main/java/gov/va/ascent/zipkin/AscentZipkinApplication.java
+++ src/main/java/gov/va/ascent/zipkin/AscentZipkinApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableZipkinServer
+@EnableZipkinStreamServer
 @EnableDiscoveryClient
 public class AscentZipkinApplication {
 
