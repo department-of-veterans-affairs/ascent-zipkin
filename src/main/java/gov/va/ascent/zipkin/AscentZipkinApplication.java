dc4c7e3e8564365471c947f6742abf8144fa113d
Raju Thota <rajuthota@Rajus-MacBook-Pro-2.local>
2018-02-05 16:56:44 -0500

    ST-1081 - Removed unused imports

--- src/main/java/gov/va/ascent/zipkin/AscentZipkinApplication.java
+++ src/main/java/gov/va/ascent/zipkin/AscentZipkinApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
-import zipkin.server.EnableZipkinServer;
-
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AscentZipkinApplication {
