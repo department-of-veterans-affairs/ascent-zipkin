559dbe359349955015fcdf102f20ed1627a110e1
Raju Thota <rajuthota@Rajus-MacBook-Pro-2.local>
2018-02-06 14:06:55 -0500

    ST-1081 - Rolling back the profile based changes.

--- src/main/java/gov/va/ascent/zipkin/AscentZipkinApplication.java
+++ src/main/java/gov/va/ascent/zipkin/AscentZipkinApplication.java
@@ -4,8 +4,10 @@ package gov.va.ascent.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
 @SpringBootApplication
+@EnableZipkinStreamServer
 @EnableDiscoveryClient
 public class AscentZipkinApplication {
 
